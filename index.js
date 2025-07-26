54d5ed7d3ff7e9d3a2242af840d2e67a496ac912
Prashant Verma <verpras@americas.manulife.net>
2025-07-26 04:24:16 -0400

    addded az dep

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
 const express = require('express');
+const { createHandler } = require('azure-function-express');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  res.send('Hello from Express on Azure Functions!');
 });
